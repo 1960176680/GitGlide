00571c25b263d3190d9b22741e017256112c0ad0
太阳 <1960176680@qq.com>
2018-07-26 16:12:24 +0800

    显示圆图片
    显示圆图片

--- app/src/main/java/com/example/administrator/gitglide/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitglide/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.administrator.gitglide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Glide.with(MainActivity.this)
+                        .load("http://img1.dzwww.com:8080/tupian_pl/20150813/16/7858995348613407436.jpg")
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(imageView);
+                 
+            }
+        });
+
     }
 }
