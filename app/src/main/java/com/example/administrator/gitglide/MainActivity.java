3026a81a949eee4b582610d2d24f0b603526bad8
太阳 <1960176680@qq.com>
2018-07-31 13:38:21 +0800

    提交
    提交

--- app/src/main/java/com/example/administrator/gitglide/MainActivity.java
+++ app/src/main/java/com/example/administrator/gitglide/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.administrator.gitglide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.request.RequestOptions;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
@@ -24,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Glide.with(MainActivity.this)
                         .load("http://img1.dzwww.com:8080/tupian_pl/20150813/16/7858995348613407436.jpg")
-                        .apply(RequestOptions.circleCropTransform())
+                        .apply(RequestOptions.placeholderOf(R.mipmap.ic_launcher))//图片加载出来前，显示的图片
+                        .apply(RequestOptions.circleCropTransform())//请求选项通过 RequestOptions 类和 apply() 方法来应用到程序中。
+                        .transition(withCrossFade())//过渡选项
                         .into(imageView);
                  
             }
